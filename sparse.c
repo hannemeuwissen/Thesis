3477844d31774a5c326f43106bb54f5b65d5f732
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-05-29 15:35:17 +0100

    (Home) Find mistake Arnoldi

--- sparse.c
+++ sparse.c
@@ -100,12 +100,10 @@ void Arnoldi(sparse_CSR A, double * b, const int len, double * Q, const int m){
     cblas_dcopy(len, b, 1, Q, 1); /* Set q0 */
     double norm_b = cblas_dnrm2(len, Q, 1);
     cblas_dscal(len, 1/norm_b, Q, 1); /* Normalize */
-    print_matrix(Q, len, m);
     double h;
     double * w = malloc(len*sizeof(double));
     for(int j=1;j < m;j++){
         spmv(A, Q + j*len, len, w);
-        print_vector(w, len);
         for(int i=1;i<=j;i++){
             h = cblas_ddot(len, w, 1, Q + i*len, 1);
             cblas_daxpy(len,-h,Q + i*len,1,w, 1);
