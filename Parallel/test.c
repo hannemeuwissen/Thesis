79a5f0c687a82dc3afc577d4707741776a0eb67b
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-06-15 11:36:52 +0100

    (Home) Try to improve performance spmv

--- Parallel/test.c
+++ Parallel/test.c
@@ -36,9 +36,9 @@ int main(int argc, char **argv)
     }
     
     /* Test SPMV */
-    int m = 8;
-    int n = 2;
-    int nnz_per_row = 2;
+    int m = 80;
+    int n = 20;
+    int nnz_per_row = 20;
     sparse_CSR M = generate_regular_graph_part_csr(n, m, nnz_per_row);
     printf("Process %d finished generating graph part of size %dx%d.\n", myid, n, m);
     // /* Print in order */
@@ -69,12 +69,12 @@ int main(int argc, char **argv)
     double t2 = MPI_Wtime();
     if(!myid){
         printf("First lines from result on process 0:\n");
-        print_vector(result, 2); // result should be 1 overall (sum of row elements)
+        print_vector(result, 20); // result should be 1 overall (sum of row elements)
         printf("Runtime: %lf\n", t2-t1);
     }
     if(myid == 3){
         printf("First lines from result on process 3:\n");
-        print_vector(result, 2); // result should be 1 overall (sum of row elements)
+        print_vector(result, 20); // result should be 1 overall (sum of row elements)
         printf("Runtime: %lf\n", t2-t1);
     }
 
