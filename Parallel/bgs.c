861f0c4d0b15e6eab45e74b9176dc6debfb8edcd
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-01 20:32:11 +0100

    (Home) Small alterations during testing

--- Parallel/bgs.c
+++ Parallel/bgs.c
@@ -51,12 +51,12 @@ void bgs(double *Q, double *V, double * R, const int m, const int n, const int s
 void bgs_on_transpose(double *Q, double *V, double * R, const int m, const int n, const int s, MPI_Comm comm){
     /* Step 1: matrix-matrix multiplication */
     double * localR = malloc(n*s*sizeof(double));
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, n, s, m, 1.0, Q, n, V, s, 0.0, localR, n);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, n, s, m, 1.0, Q, m, V, m, 0.0, localR, s);
 
     /* Step 2: Communicate - Allreduce for each element */
     MPI_Allreduce(localR, R, n*s, MPI_DOUBLE, MPI_SUM, comm);
     free(localR);
 
     /* Step 3: Local Gramm-Schmidt step - V <- V - QR */
-    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, m, s, n, -1.0, Q, m, R, s, 1.0, V, m);
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, m, s, n, -1.0, Q, n, R, n, 1.0, V, s);
 }
