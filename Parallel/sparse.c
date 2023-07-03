70f418c1d45804f7edd71d3c480870766ab7de3b
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-03 12:36:49 +0100

    (Home) Find problem spmv

--- Parallel/sparse.c
+++ Parallel/sparse.c
@@ -278,6 +278,7 @@ void spmv(sparse_CSR A, double * x, double len, double * result, const int myid,
 void matrix_powers(sparse_CSR A, double * start_v, double * V, const int s, const int m, const int myid, const int nprocs, int *start, int *end, MPI_Comm comm){
     printf("Before spmv\n");
     spmv(A, start_v, m, V, myid, nprocs, start, end, comm);
+    printf("After first spmv\n");
     for(int k=1;k<s;k++){
         spmv(A, V + (s-1)*m, m, V + s*m, myid, nprocs, start, end, comm);
     }
