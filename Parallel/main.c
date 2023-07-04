8c4ad45df1ae3d2fffd1435687cd1e816ee9481f
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-04 12:26:48 +0100

    (Home) Test by generating

--- Parallel/main.c
+++ Parallel/main.c
@@ -54,16 +54,12 @@ int main(int argc, char **argv){
         }
     }
 
-    printf("Here!\n");
-
     /* Broadcast input to all processes */
     MPI_Bcast(&degree, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&s, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&nnz, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(filename_v, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
-
-    printf("Input read!\n");
 
     /* Determine the start index and size of part for calling process */
     int * start = malloc(nprocs*sizeof(int));
@@ -73,7 +69,6 @@ int main(int argc, char **argv){
 
     /* Generate part of transition matrix for calling process */
     sparse_CSR A = generate_regular_graph_part_csr(m, M, nnz);
-    printf("Graph generated!\n");
 
     // /* Test: read from file (each process)*/
     // sparse_CSR A;
