15b302bcca37eced3b9ffa9d77106b1362c5a4e2
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-11 10:42:29 +0100

    (Home) Try fixing spmv by using pscw

--- Parallel/sparse.c
+++ Parallel/sparse.c
@@ -162,10 +162,15 @@ void spmv(sparse_CSR A, double * x, double len, double * result, const int myid,
     MPI_Win_create(x, len*sizeof(double), sizeof(double), MPI_INFO_NULL, comm, &win);
 
     MPI_Win_post(world_group, 0, win);
+
+    MPI_Win_start(world_group, 0, win);
     
     for(int i=0;i<len;i++){ /* For all rows: gather elements + dot product of row and gathered elements */
         
         printf("Calculating row %d\n", i);
+
+        MPI_Win_post(world_group, 0, win);
+
         /* Gather elements */
         int nnz_i = 0;
         // MPI_Win_fence(MPI_MODE_NOPRECEDE | MPI_MODE_NOPUT | MPI_MODE_NOSTORE,win);
@@ -184,9 +189,13 @@ void spmv(sparse_CSR A, double * x, double len, double * result, const int myid,
         // MPI_Win_fence(MPI_MODE_NOSUCCEED | MPI_MODE_NOSTORE | MPI_MODE_NOPUT,win);
         MPI_Win_complete(win);
 
+        MPI_Win_wait(win);
+
         /* Dot product */
         result[i] = cblas_ddot(nnz_i, A.values + A.rowptrs[i], 1, x_gathered_elements, 1);
     }
+
+    MPI_Win_complete(win);
 
     MPI_Win_wait(win);
 
