71463c7248a21ee9a93f6fdfcd8fd2bc5320848c
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-11 12:07:54 +0100

    (Home) Find why timing bgs is -inf

--- Parallel/main.c
+++ Parallel/main.c
@@ -235,9 +235,9 @@ int main(int argc, char **argv){
         // printf("Part of Q process 0:\n");
         // print_matrix_transposed(mathcalQ, (steps*s + 1), m);
         printf("Total runtime process %d: %lf\n", myid, t2 - t1);
-        printf("Average time matrix powers process %d: %lf\n", myid, average(mp_times, block - 1));
-        printf("Average time block (classical) Gram-Schmidt process %d: %lf\n", myid, average(bgs_times, block - 2));
-        printf("Average time TSQR process %d: %lf\n", myid, average(tsqr_times, block - 1));
+        printf("Average time matrix powers process %d: %lf\n", myid, average(mp_times, block));
+        printf("Average time block (classical) Gram-Schmidt process %d: %lf\n", myid, average(bgs_times, block - 1));
+        printf("Average time TSQR process %d: %lf\n", myid, average(tsqr_times, block));
         printf("Upper Hessenberg: %lf\n", average(hess_times, block - 1));
         printf("Hessenberg:\n");
         print_matrix(mathcalH, (steps*s + 1), steps*s);
@@ -245,9 +245,9 @@ int main(int argc, char **argv){
 
     MPI_Barrier(MPI_COMM_WORLD);
     printf("Total runtime process %d: %lf\n", myid, t2 - t1);
-    printf("Average time matrix powers process %d: %lf\n", myid, average(mp_times, block - 1));
-    printf("Average time block (classical) Gram-Schmidt process %d: %lf\n", myid, average(bgs_times, block - 2));
-    printf("Average time TSQR process %d: %lf\n", myid, average(tsqr_times, block - 1));
+    printf("Average time matrix powers process %d: %lf\n", myid, average(mp_times, block));
+    printf("Average time block (classical) Gram-Schmidt process %d: %lf\n", myid, average(bgs_times, block - 1));
+    printf("Average time TSQR process %d: %lf\n", myid, average(tsqr_times, block));
 
     if(myid == 1){ /* Print out results */
         // printf("Part of Q process 1:\n");
