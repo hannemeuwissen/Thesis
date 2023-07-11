12757e83bcafdb9a52c22711e920023adde290df
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-11 10:31:17 +0100

    (Home) Try fixing spmv by using pscw

--- Parallel/sparse.c
+++ Parallel/sparse.c
@@ -189,7 +189,7 @@ void spmv(sparse_CSR A, double * x, double len, double * result, const int myid,
         result[i] = cblas_ddot(nnz_i, A.values + A.rowptrs[i], 1, x_gathered_elements, 1);
     }
 
-    MPI_Win_complete(win;)
+    MPI_Win_complete(win);
 
     MPI_Win_wait(win);
 
