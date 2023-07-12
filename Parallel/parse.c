ebd71297834254e482908bfe04fd0326735d27ef
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-12 11:26:40 +0100

    (Home) Reset degree subspace

--- Parallel/parse.c
+++ Parallel/parse.c
@@ -50,7 +50,6 @@ void parse_command_line_regular(const int argc, char * const *argv, int * M, int
                     printf("Usage : ./main [-v filename_v] [-m nr of nodes] [-z nnz per row] [-d degree] [-s blocksize]\n");
                     MPI_Abort(comm, 1);
                 }
-                (*degree)--;
                 break;
             case 's':
                 if(sscanf(optarg,"%d",s) == 0){
@@ -119,7 +118,6 @@ void parse_command_line_irregular(const int argc, char * const *argv, char * fil
                     printf("Usage : ./main [-f filename_A] [-v filename_v] [-m nr of nodes] [-z min nnz] [-x max nnz] [-d degree] [-s blocksize]\n");
                     MPI_Abort(comm, 1);
                 }
-                (*degree)--;
                 break;
             case 's':
                 if(sscanf(optarg,"%d",s) == 0){
