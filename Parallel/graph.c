42810de79a86f30e5e0ee45e52153d4b501e0631
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-14 12:39:06 +0100

    (Home) find mistake irr graph generator

--- Parallel/graph.c
+++ Parallel/graph.c
@@ -198,6 +198,7 @@ sparse_CSR generate_irregular_csr(const int M, const int min_nnz, const int max_
     T.ncols = M;
     T.rowptrs = malloc((M+1)*sizeof(int));
     int *nnz_per_row = malloc(M*sizeof(int));
+    printf("Here!\n");
     random_nnz_per_row(nnz_per_row, &(T.nnz), min_nnz, max_nnz, M);
     int sorter(const void * f1, const void * f2){return (*(int*)f2 - *(int*)f1);}
     qsort(nnz_per_row, T.nnz, sizeof(int), sorter);
