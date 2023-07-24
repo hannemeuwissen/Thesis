500e8d4d1402bbdf352f2ea9f62c6fa429341b80
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-24 16:33:20 +0100

    (Home) Add script that measures grassmann distance

--- Parallel/angle_subspaces.c
+++ Parallel/angle_subspaces.c
@@ -110,6 +110,8 @@ int main(int argc, char **argv){
     double dist = grassmann_distance(S, n);
 
     printf("The Grassmann distance between the subspaces in %s and %s is:\n%e\n", argv[1], argv[2], dist);
+    printf("\n");
+    printf("Max distance: %lf\n (pi/2)\n", M_PI_2)
 
     free(Q1);
     free(Q2);
