b26d3ed26e66ed188ab1c89f8b649480dff38e8d
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-07-24 16:09:58 +0100

    (Home) Add script that measures grassmann distance

--- Parallel/angle_subspaces.c
+++ Parallel/angle_subspaces.c
@@ -91,10 +91,10 @@ int main(int argc, char **argv){
     cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, n, n, M, 1.0, Q1, n, Q2, n, 0.0, D, n);
     
     /* Singular value decomposition of D */
-    double * S = malloc(M*sizeof(double));
+    double * S = malloc(n*sizeof(double));
     double U,V;
     double * superb = malloc((n-2)*sizeof(double));
-    int ret = LAPACKE_dgesvd(CblasRowMajor, 'N', 'N', n, n, D, n, S, &U, 1, &V, 1, superb);
+    int ret = LAPACKE_dgesvd(CblasRowMajor, 'N', 'N', n, n, M, D, n, S, &U, 1, &V, 1, superb);
     if(ret!=0){
         if(ret<0){
             fprintf(stderr, "LAPACKE_dgesvd failed. Parameter %d had an illegal value\n", abs(ret));
