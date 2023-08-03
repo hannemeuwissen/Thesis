cebc79642c5707dc27bb2bc6cd093532276a004c
U-LAPTOPHANNE\Gebruiker <hanne.meuwissen@gmail.com>
2023-08-03 11:30:42 +0100

    (Home) Small change jobscript

--- Jobscripts/small_test.sh
+++ Jobscripts/small_test.sh
@@ -14,9 +14,8 @@ cd /home/users/mschpc/2022/meuwissh/Thesis/Parallel
 
 # load up necessary modules
 # module load cports gcc/12.1.0-gnu intel openmpi
-module load apps intel-oneapi/2022.1.0 
-module load gcc/9.3.0 
-module load intel/18.0.5 openmpi/3.1.6
+# module load apps intel-oneapi/2022.1.0 
+module load gcc/12.2.0 intel/18.0.5 openmpi/3.1.6
 
 # launch code
 make
